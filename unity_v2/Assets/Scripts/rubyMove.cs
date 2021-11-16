209db91627b9e1719baf9ceb0b336ad18aab95a4
hgook120 <55180627+hgook120@users.noreply.github.com>
2021-11-16 16:09:37 +0800

    20211116_02

--- unity_v2/Assets/Scripts/rubyMove.cs
+++ unity_v2/Assets/Scripts/rubyMove.cs
@@ -7,21 +7,43 @@ public class rubyMove : MonoBehaviour
 
     public float moveSpeed;
 
+    public Rigidbody2D ruby;
+
+    public Animator rubyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
+        ruby = GetComponent<Rigidbody2D>();
         
+        rubyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 rubyPosition = transform.position;
-        rubyPosition.x = rubyPosition.x + moveSpeed * Input.GetAxis("Horizontal");
-        print( Input.GetAxis("Horizontal") );
-        transform.position = rubyPosition;
-        rubyPosition.y = rubyPosition.y + moveSpeed * Input.GetAxis("Vertical");
-        print(Input.GetAxis("Vertical"));
-        transform.position = rubyPosition;
+
+        //rubyPosition.x = rubyPosition.x + moveSpeed * Input.GetAxis("Horizontal");
+        //print( Input.GetAxis("Horizontal") );
+        //transform.position = rubyPosition;
+        //rubyPosition.y = rubyPosition.y + moveSpeed * Input.GetAxis("Vertical");
+        //print(Input.GetAxis("Vertical"));
+        //transform.position = rubyPosition;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal ==0 && vertical ==0)
+        {
+            rubyAnimator.SetTrigger("stand");
+        }
+
+        rubyAnimator.SetFloat("moveX", horizontal);
+        rubyAnimator.SetFloat("moveY", vertical);
+        rubyPosition.x += moveSpeed * horizontal * Time.deltaTime;
+        rubyPosition.y += moveSpeed * vertical * Time.deltaTime;
+
+        ruby.MovePosition(rubyPosition);       
     }
 }
